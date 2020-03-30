75b00c8adc3e0050a4a533932b070876f518607c
Aileen Cai <Aileen.Cai@tdameritrade.com>
2020-03-30 15:11:21 -0400

    fetch weatherAPI data (5day-3hr forecast)

--- src/WeekContainer.js
+++ src/WeekContainer.js
@@ -1,6 +1,18 @@
 import React from 'react';
+import apiConfig from './apiKeys';
+
+
 
 class WeekContainer extends React.Component {
+  componentDidMount = () =>{
+    const weatherURL =
+        `http://api.openweathermap.org/data/2.5/forecast?zip=11102&units=imperial&APPID=${apiConfig.weatherKey}`
+    fetch(weatherURL)
+        .then(res => res.json())
+        .then(data => console.log("Data List Loaded", data.list))
+  }
+
+  
   render() {
     return (
       <div>
